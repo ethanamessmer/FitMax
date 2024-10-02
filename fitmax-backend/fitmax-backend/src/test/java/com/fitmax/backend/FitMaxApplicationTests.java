f28b3492625411bc4c3f24e37ba6fab3bd0bd822
ethanamessmer <158070606+ethanamessmer@users.noreply.github.com>
2024-10-01 21:29:49 -0500

    Set up Springboot for the backend

---
+++ fitmax-backend/fitmax-backend/src/test/java/com/fitmax/backend/FitMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmax.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
